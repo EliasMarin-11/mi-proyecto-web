f0c6b6aae817853f8bcb2221a40b9bd3edc9b72b
Alejajandro05 <alejandro.mentado101@alu.ulpgc.es>
2026-04-17 16:35:35 +0100

    Header añadido, esta mal pero tenemos la base

--- angular_sprint3/src/app/app.ts
+++ angular_sprint3/src/app/app.ts
@@ -1,16 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-// 1. IMPORTA EL COMPONENTE FÍSICAMENTE
+// --- 1. IMPORTAMOS LAS PIEZAS ---
+import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  // 2. REGÍSTRALO AQUÍ PARA QUE EL HTML LO ENTIENDA
-  imports: [RouterOutlet, FooterComponent],
+  // --- 2. DAMOS PERMISO PARA USARLOS ---
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('elias_crak');
-  protected readonly descripcion = signal('Mi primera práctica con componentes');
+  // Ya puedes borrar esto si quieres limpiar la vista
+  protected readonly title = signal('What\'s in your Fridge');
 }
---
+++ angular_sprint3/src/app/components/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-header',
+    imports: [], // Como es Standalone, aquí pondremos herramientas extra más adelante
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+    // Por ahora esto está vacío.
+    // ¡Aquí meteremos la lógica del buscador más adelante!
+}
