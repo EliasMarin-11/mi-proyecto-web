1cf87e361a7f2fe285e708bcfa8528db5b35a6c5
pablo <pablo.campos102@alu.ulpgc.es>
2026-04-28 13:02:40 +0100

    CREACION ARCHIVOS PÁINAS BASE

--- angular_sprint3/src/app/homepage.ts
+++ angular_sprint3/src/app/homepage.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { BuscadorComponent} from './components/buscador/buscador.component';
+import { Tarjeta_receta_verticalComponent} from './components/tarjeta_receta_vertical/tarjeta_receta_vertical.component';
 
 @Component({
   selector: 'homepage',
   // --- 2. DAMOS PERMISO PARA USARLOS ---
-  imports: [HeaderComponent, FooterComponent, BuscadorComponent],
+  imports: [HeaderComponent, FooterComponent, BuscadorComponent, Tarjeta_receta_verticalComponent],
   templateUrl: './homepage.html',
 })
 export class Homepage {
